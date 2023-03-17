61b9f7698151d3647a0177eafe95714a5792330b
Austin Rooks <Austionian@users.noreply.github.com>
2023-03-17 09:09:22 -0500

    switch to pnpm

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,23 +1,28 @@
-<div class='mt-20 max-w-lg text-center m-auto'>
-<h1 class='text-3xl font-bold text-white mb-3'>Boozhoo!</h1>
-<p class='text-white mb-20 text-left'>This app calculates your safe consumption levels of fish that are caught in the 1836 Treaty territories monitored by the Chippewa-Ottawa Resource Authority!</p>
+<script>
+    import bg from '$lib/images/fish-bg.jpg'
+</script>
 
-<label for='email' class='text-white text-sm float-left'>Email Address</label>
-<br/>
-<input type='email' id='email' class='w-full rounded-md p-2 mb-3'/>
-<div class='w-full flex justify-center'>
-<div class='w-full text-center flex justify-center flex-col'>
-    <button class='text-white bg-teal-700 rounded-md px-8 py-2 w-full mb-3'>SIGN UP</button>
-    <h2>
-        <a href='/' class='text-teal-700 text-2xl font-bold w-full'>
-            CONTINUE AS GUEST
-        </a>
-    </h2>
+<svelte:head>
+    <title>Login</title>
+    <meta name='description' content="Login to the fish app" />
+</svelte:head>
+
+<img alt="fishy background" src={bg} class="fixed -z-10 h-screen w-screen" />
+<div class='mt-20 max-w-lg text-center m-auto'>
+    <h1 class='text-3xl font-bold text-white mb-3'>Boozhoo!</h1>
+    <p class='text-white mb-20 text-left'>This app calculates your safe consumption levels of fish that are caught in the 1836 Treaty territories monitored by the Chippewa-Ottawa Resource Authority!</p>
+
+    <label for='email' class='text-white text-sm float-left'>Email Address</label>
+    <br/>
+    <input type='email' id='email' class='w-full rounded-md p-2 mb-3'/>
+    <div class='w-full flex justify-center'>
+    <div class='w-full text-center flex justify-center flex-col'>
+        <button class='text-white bg-teal-700 rounded-md px-8 py-2 w-full mb-3'>SIGN UP</button>
+        <h2>
+            <a href='/' class='text-teal-700 text-2xl font-bold w-full'>
+                CONTINUE AS GUEST
+            </a>
+        </h2>
+    </div>
+    </div>
 </div>
-</div>
-</div>
-<style lang='postcss'>
-    :global(html) {
-            background-image: url('/images/fish-bg.jpg')
-        }
-</style>
